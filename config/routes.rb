a5bace489fe10cde8a601d2c626a5573e0f6cc6f
jblang94 <jbarlang@gmail.com>
2016-07-19 04:16:40 +0000

    completed step 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   
-  get 'sessions/new'
-
-  get 'sessions/create'
-
   resources :sessions, only: [:new, :create, :destroy]
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   
   root 'movies#index'
