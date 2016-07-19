b6faeaf845140385bcc062a10c7bf48c97eecc1b
jblang94 <jbarlang@gmail.com>
2016-07-19 03:24:49 +0000

    completed step 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  resources :sessions, only: [:new, :create]
   resources :movies
   resources :users, only: [:new, :create]
   
