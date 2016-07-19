62536cd2a4eab41480971213c0d5af053f8ed62d
jblang94 <jbarlang@gmail.com>
2016-07-19 19:17:12 +0000

    able to render /admin/users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create]
+
+  namespace :admin do
+    resources :users
+  end
   
   root 'movies#index'
   
