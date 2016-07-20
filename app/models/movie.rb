d489a2ab54c4da16fb7f90f283d604b6900a8268
jblang94 <jbarlang@gmail.com>
2016-07-20 04:47:52 +0000

    title and director search working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,6 +12,11 @@ class Movie < ApplicationRecord
 
   mount_uploader :poster, PosterUploader
 
+  def self.search(title_search, director_search)
+    return Movie.all if title_search.blank? && director_search.blank?
+    Movie.where('title LIKE ? AND director LIKE ?', "%#{title_search}%", "%#{director_search}%")
+  end
+
   def review_average
     return 0 if reviews.size == 0
     reviews.sum(:rating_out_of_ten)/reviews.size
