1af259a2e14f3a06e635ff50ab4fc6b6873b9ac3
jblang94 <jbarlang@gmail.com>
2016-07-20 06:15:15 +0000

    implemented admin create users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,4 +10,23 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "Successfully created new user!"
+    else
+      render :new
+    end
+  end
+
+  protected 
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :is_admin)
+  end
+
 end
