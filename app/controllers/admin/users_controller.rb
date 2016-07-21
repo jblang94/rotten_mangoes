7147fb38b93ae036e92b5db324a96a1493d83d12
jblang94 <jbarlang@gmail.com>
2016-07-21 22:52:00 +0000

    added pagination, seeding data, refactored masquerading

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,8 +3,7 @@ class Admin::UsersController < ApplicationController
   before_filter :restrict_admin_access
   
   def index
-    # @users = User.all.page(params[:page])
-    @users = User.all
+    @users = User.order(:lastname).page(params[:page]).per(10)
   end
 
   def show
